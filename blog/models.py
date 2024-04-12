a6a2bd4532734d36a85cf60ded641fb871a62a43
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-04-13 00:02:25 +0500

    news add

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     image = models.ImageField(upload_to='posts')
     is_published = models.BooleanField(default=False)
@@ -36,16 +46,6 @@ class Contact(models.Model):
     subject = models.CharField(max_length=100)
 
     is_solved = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,66 @@
-from django.shortcuts import render
+import requests
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from blog.models import Post, Contact, Comment
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(is_published=True)
+    d = {
+        'posts': posts,
+        'home': 'active'
+    }
+    return render(request, 'index.html', context=d)
 
 
 def blog_view(request):
-    return render(request, 'blog.html')
+    data = request.GET
+    cat = data.get('cat')
+    page = data.get('page', 1)
+    if cat:
+        posts = Post.objects.filter(is_published=True, category_id=cat)
+    else:
+        posts = Post.objects.filter(is_published=True)
+    post_obj = Paginator(posts, 3)
+    d = {
+        'posts': post_obj.page(page),
+        'blog': 'active'
+    }
+    return render(request, 'blog.html', context=d)
 
 
-def blog_single_view(request):
-    render(request, 'blog-single.html')
+def blog_single_view(request, pk):
+    post = Post.objects.filter(id=pk).first()
+    if request.method == 'POST':
+        data = request.POST
+        obj = Comment.objects.create(post_id=pk, name=data.get('name'), message=data.get('message'),
+                                     email=data.get('email'))
+        obj.save()
+        return redirect(f'/blog/{pk}')
+    comments = Comment.objects.filter(post_id=pk)
+    d = {
+        'post': post,
+        'comments': comments
+    }
+    return render(request, 'blog-single.html', context=d)
 
 
 def about_view(request):
-    return render(request, 'about.html')
+    d = {
+        'about': 'active'
+    }
+    return render(request, 'about.html', context=d)
 
 
 def contact_view(request):
+    if request.method == 'POST':
+        data = request.POST
+        obj = Contact.objects.create(name=data.get('name'), email=data.get('email'), message=data.get('message'),
+                                     subject=data.get('subject'))
+        obj.save()
+        token = "6912718237:AAH2v2r4x2TuYnHqfpbi1ci43AxYKEiBWoE"
+        requests.get(
+            f"""https://api.telegram.org/bot{token}/sendMessage?chat_id=5093765356&text=MOOSE\nid: {obj.id}\nname: {obj.name}\nemail: {obj.email}""")
+
+        return redirect('/contact')
     return render(request, 'contact.html')
